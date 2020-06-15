9d995b932cc004cc1caa70d6db14929ebca7714b
ajiang02 <ajiang0202@gmail.com>
2020-06-15 18:15:49 +0800

    Update Test.php

--- app/index/controller/Test.php
+++ app/index/controller/Test.php
@@ -24,35 +24,40 @@ class Test extends TestCase
         $node2->left  = $node3;
         $node2->right = $node4;*/
 
-        $pRoot = new TreeNode(1);
-        $node1 = new TreeNode(2);
-        $node2 = new TreeNode(3);
 
-        $pRoot->right = $node1;
-        $node1->right = $node2;
-
-
-        $res = $this->IsBalanced_Solution($pRoot);
-        $this->assertEquals(false, $res);
+        $res = $this->FindContinuousSequence(100);
+        print_r($res);
+        //$this->assertEquals(2, count($res));
     }
 
 
-    function IsBalanced_Solution($pRoot)
+    function FindContinuousSequence($target)
     {
+        $left = $right = 1;                 // 滑动窗口的左右边界初始化为 1
+        $sum  = 0;                          // 滑动窗口内的所有元素总值
+        $end  = ceil($target / 2);   // 窗口的右边界最大到 target 值的一半
+        $res  = [];                         // 存放所有满足条件的序列
 
-        return $this->recursive($pRoot) != -1;
-
-    }
-
-    function recursive($pRoot)
-    {
-        if ($pRoot == NULL) return 0;
-        $left = $this->recursive($pRoot->left);
-        if ($left == -1) return -1;
-
-        $right = $this->recursive($pRoot->right);
-        if ($right == -1) return -1;
-
-        return abs($left - $right) < 2 ? max($left, $right) + 1 : -1;
+        while ($right <= $end) {
+            // 如果窗口内总值小于目标值，则计入总值，并右边界右移
+            if ($sum < $target) {
+                $sum = $sum + $right;
+                $right++;
+            } elseif ($sum > $target) {
+                // 如果窗口总值大于目标值，则从总值中减去，并左边界右移
+                $sum = $sum - $left;
+                $left++;
+            } else {
+                // 如果窗口总值等于目标值，则放入结果数组，继续移动
+                $tmp = [];  // 存放每一次的连续序列
+                for ($i = $left; $i < $right; $i++) {
+                    $tmp[] = $i;
+                }
+                $res[] = $tmp;
+                $sum   = $sum - $left;
+                $left++;
+            }
+        }
+        return $res;
     }
 }
