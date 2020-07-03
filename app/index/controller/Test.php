e744bf6eed90a6c9ab919056c744f3cbb8ef0642
ajiang02 <ajiang0202@gmail.com>
2020-07-03 18:13:08 +0800

    Update Test.php

--- app/index/controller/Test.php
+++ app/index/controller/Test.php
@@ -25,39 +25,17 @@ class Test extends TestCase
         $node2->right = $node4;*/
 
 
-        $res = $this->FindContinuousSequence(100);
+        $str = "abcXYZdef";
+        $res   = $this->LeftRotateString($str, 3);
         print_r($res);
         //$this->assertEquals(2, count($res));
     }
 
 
-    function FindContinuousSequence($target)
+    function LeftRotateString($str, $n)
     {
-        $left = $right = 1;                 // 滑动窗口的左右边界初始化为 1
-        $sum  = 0;                          // 滑动窗口内的所有元素总值
-        $end  = ceil($target / 2);   // 窗口的右边界最大到 target 值的一半
-        $res  = [];                         // 存放所有满足条件的序列
-
-        while ($right <= $end) {
-            // 如果窗口内总值小于目标值，则计入总值，并右边界右移
-            if ($sum < $target) {
-                $sum = $sum + $right;
-                $right++;
-            } elseif ($sum > $target) {
-                // 如果窗口总值大于目标值，则从总值中减去，并左边界右移
-                $sum = $sum - $left;
-                $left++;
-            } else {
-                // 如果窗口总值等于目标值，则放入结果数组，继续移动
-                $tmp = [];  // 存放每一次的连续序列
-                for ($i = $left; $i < $right; $i++) {
-                    $tmp[] = $i;
-                }
-                $res[] = $tmp;
-                $sum   = $sum - $left;
-                $left++;
-            }
-        }
-        return $res;
+        $sub = substr($str,0,$n);
+        $truncated_str = substr($str,$n);
+        return $truncated_str.$sub;
     }
 }
